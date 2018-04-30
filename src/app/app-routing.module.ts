86e65d10b4b5f6ca79c9a08281c0955aba617ba1
ChrisSensebe <christophe.sensebe@gmail.com>
2018-04-30 22:59:14 +0200

    adding app-routing.module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import {LandingComponent} from './components/landing/landing.component';
+import {SignUpComponent} from './components/sign-up/sign-up.component';
+import {LogInComponent} from './components/log-in/log-in.component';
+import {RouterModule} from '@angular/router';
+
+const routes = [
+  { path: 'log-in', component: LogInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: '', component: LandingComponent },
+  { path: '**', redirectTo: '/' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
